e84bf76b41d36df852e488518e3c45cea9f14865
cg2lab <cg2lab@DESKTOP-HCES3GB>
2019-10-17 20:17:28 +0800

    15472

---
+++ Test01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNum = random.Next(5) + 3;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
